47d951a256ec68e6b4bd4535b875f249731b27ae
tazadejava <tonitruirrein@gmail.com>
2021-02-17 01:50:16 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,19 +1,6 @@
 # Blueprint 2021 Workshop Project
 
-Goal: by the end of the 3 workshop sessions, the students will be able to re-create this project.
-
-Topics learned:
-
-- Create an android project
-- Design an interface using the ConstraintLayout and common widgets (Button, TextView, EditView, Spinner)
-- Load activities through Intents with extra data
-- Use commonly used basic data structures (HashMap, Set, List)
-- Initialize and manipulate a RecyclerView (Adapter, ViewHolder, LayoutManager)
-- Use the Picasso library to load images
-- Use the GSON library to load JSON files
-- Create a basic search and sorting system
-
-***
+Completed up to session 1. Starter code pack for session 2.
 
 This example project uses the Pomological Watercolor Collection:
 
---
+++ app/src/main/java/org/hackmit/workshopproject/MainActivity.java
@@ -0,0 +1,115 @@
+package org.hackmit.workshopproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<String> ids;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //define our textviews and imageviews
+
+        TextView myText1 = findViewById(R.id.myText1);
+        TextView myText2 = findViewById(R.id.myText2);
+        TextView myCustomText = findViewById(R.id.myCustomText);
+
+        ImageView myImage1 = findViewById(R.id.myImage1);
+        ImageView myImage2 = findViewById(R.id.myImage2);
+        ImageView myImage3 = findViewById(R.id.myImage3);
+
+        setJournalEntry(myText1, myImage1, "4343");
+        setJournalEntry(myText2, myImage2, "5499");
+
+        setJournalEntry(myCustomText, myImage3, "7580");
+        //overwrite with text
+        myCustomText.setText("Empty journal entry");
+
+        EditText myCustomEditText = findViewById(R.id.myCustomEditText);
+        Button confirmEditTextButton = findViewById(R.id.confirmEditTextButton);
+        confirmEditTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCustomText.setText(myCustomEditText.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Wrote a new journal entry!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button randomizeEntriesButton = findViewById(R.id.randomizeEntriesButton);
+        randomizeEntriesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setJournalEntry(myText1, myImage1, ids.get((int) (Math.random() * ids.size())));
+                setJournalEntry(myText2, myImage2, ids.get((int) (Math.random() * ids.size())));
+                setJournalEntry(myCustomText, myImage3, ids.get((int) (Math.random() * ids.size())));
+                myCustomText.setText("Empty journal entry");
+            }
+        });
+
+        //load ids
+        try {
+            ids = new ArrayList<>();
+            String[] fileNames = getAssets().list("Datapack/data/");
+
+            for(String fileName : fileNames) {
+                String id = fileName.substring(0, fileName.lastIndexOf("."));
+                ids.add(id);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setJournalEntry(TextView textView, ImageView imageView, String id) {
+        try {
+            Gson gson = new Gson();
+            InputStreamReader reader = new InputStreamReader(getAssets().open("Datapack/data/" + id + ".json"));
+            JsonObject data = gson.fromJson(reader, JsonObject.class);
+            reader.close();
+
+            String name = data.get("Name").getAsString();
+            String year = data.has("Year") ? data.get("Year").getAsString() : "Unknown year";
+            String commonName = data.has("Common name") ? data.get("Common name").getAsString() : "Unknown common name";
+
+            textView.setText(name + ", " + year + "\n" + commonName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Picasso.get().load("file:///android_asset/Datapack/images/" + id + ".jpg").fit().centerInside().into(imageView);
+
+        //create click listener to go to separate activity
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent showFoodItemIntent = new Intent(MainActivity.this, FoodItemActivity.class);
+                showFoodItemIntent.putExtra("id", id);
+                showFoodItemIntent.putExtra("desc", textView.getText().toString());
+                startActivity(showFoodItemIntent);
+            }
+        });
+    }
+}
